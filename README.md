26d55d5ce3c660e91233302457bbb094d1161317
Jesper Jensen <delusionalLogic@gmail.com>
2014-08-05 01:58:43 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 lolot
 =====
 
-Lot of lines of terrible is a different lua class system that focuses on avoiding repeated verbose statements.
+"Lot Of Lines Of Terrible" is a different lua class system that focuses on avoiding repeated verbose statements.
+
+Usage
+-----
+
+lolot is uses a simple syntax although it does have a few quirks due to lua limitations.
+
+The first step is as always to ```require``` the file: ```require "lolot"```
+
+You then need to declare a class. This is done with a call to the function ```class()``` with 2 required parameters. The first being the name of the class, and the second being the function encapsulating the namespace. The function will then return the definition, which you need to catch in a variable. This look like:
+
+```
+local Person = class("Person", function() end)
+```
+
+To add functionality to your newly created class you need to place it in the namespace function. This is where the elegance of lolot begins.
+
+To add a new local variable you simply define it within the scope, the same goes with functions. A function will dynamically have a self variable set when called, so there is no need to have it as an argument. You could therefore make a person class that encapsulates a persons age as simply as:
+
+```
+local Person = class("Person", function()
+	age = 0
+
+	function __init__(birthYear)
+		self.age = 2014 - birthYear
+	end
+
+	function setAge(age)
+		self.age = age
+	end
+
+	function __tostring__()
+		return tostring(self.age)
+	end
+
+	function printAge()
+		print(self.age)
+	end
+end)
+```
+
+Things you might notice if you have worked with other lua class systems is that we don't type the name of the variable before the function definitions, and local instance variables just work. When you declare instance variables you should remember that none of the lua functions are available, so if you need to assign variables values from function calls you should assign them in the constructor.
+
+Another thing you might want to do is take advantage of lua metatable events. lolot redirects all these to methods of the same name, except if follows the python way of symmetry with the underscores. One exception is ```__init__()``` which is the constructor. You should notice that all variables declared globally in the namespace are automatically constructed on instantiation. An example use of the given class would look like:
+
+```
+Oscar = Person(2010)
+Oscar:printAge() --> prints 4
+```
---
+++ lolot.lua
@@ -0,0 +1,128 @@
+local function captureGlobals(f)
+	local newG = {}
+	local oldG = _ENV
+	_ENV = newG
+	f()
+	_ENV = oldG
+	return newG
+end
+
+local function splitClass(class)
+	local functions = {}
+	local variables = {}
+
+	for k,v in pairs(class) do
+		if type(v) == "function" then
+			functions[k] = v
+		else
+			variables[k] = v
+		end
+	end
+
+	return functions, variables
+end
+
+local function deepcopy(table, dest)
+	local dest = dest or {}
+
+	for k,v in pairs(table) do
+		if type(v) == "table" then
+			dest[k] = deepcopy(v)
+		else
+			dest[k] = v
+		end
+	end
+end
+
+local function invokeWithSelf(self, f, ...)
+	local oldSelf = _ENV["self"]
+	_ENV["self"] = self
+	local retVal = f(...)
+	_ENV["self"] = oldSelf
+	return retVal
+end
+
+local function setSelfInvoker(f)
+	return function(self, ...)
+		return invokeWithSelf(self, f, ...)
+	end
+end
+
+local function copyFunctionsTap(src, dst, tap)
+	for k,v in pairs(src) do
+		if type(v) == "function" then
+			dst[k] = tap(v)
+		end
+	end
+end
+
+local function safeCallAlternative(funcName, alternative)
+	return function(self, ...)
+		if self[funcName] then
+			return invokeWithSelf(self, self[funcName], self, ...)
+		else
+			if alternative then
+				return alternative(self, ...)
+			end
+		end
+	end
+end
+
+local instmt = {
+	__tostring = safeCallAlternative("__tostring__", function(self, ...)
+		return ("Instance<%s>"):format(self.__name)
+	end),
+
+	__call = safeCallAlternative("__call__"),
+	__len = safeCallAlternative("__len__"),
+	__unm = safeCallAlternative("__unm__"),
+	__add = safeCallAlternative("__add__"),
+	__sub = safeCallAlternative("__sub__"),
+	__mul = safeCallAlternative("__mul__"),
+	__div = safeCallAlternative("__div__"),
+	__mod = safeCallAlternative("__mod__"),
+	__pow = safeCallAlternative("__pow__"),
+	__concat = safeCallAlternative("__concat__"),
+	__eq = safeCallAlternative("__eq__"),
+	__lt = safeCallAlternative("__lt__"),
+	__le = safeCallAlternative("__le__"),
+}
+
+local mt = {
+	__call = function(self, ...)
+		return self:new(...)
+	end,
+
+	__tostring = function(self)
+		return ("Class<%s>"):format(self.name)
+	end,
+}
+
+local function instantiate(class, ...)
+	local inst = {
+		__name = class.name
+	}
+
+	deepcopy(class.variables, inst)
+	copyFunctionsTap(class.functions, inst, setSelfInvoker)
+
+	if inst.__init__ then
+		inst:__init__(...)
+	end
+	return setmetatable(inst, instmt)
+end
+
+function class(name, f)
+	local classData = captureGlobals(f)
+	local functions, variables = splitClass(classData)
+
+	local classTable = {
+		name = name,
+		functions = functions,
+		variables = variables,
+	}
+
+	classTable.new = instantiate
+
+	return setmetatable(classTable, mt)
+end
